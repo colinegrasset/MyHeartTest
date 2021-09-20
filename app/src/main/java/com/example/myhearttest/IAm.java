5f4a9a47de5abbefb83598513f0458ab9c699e1b
claire_bouillot <claire.bouillot12@gmail.com>
2021-09-20 17:28:54 +0200

    Premiere page

---
+++ app/src/main/java/com/example/myhearttest/IAm.java
@@ -0,0 +1,14 @@
+package com.example.myhearttest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IAm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_iam);
+    }
+}
