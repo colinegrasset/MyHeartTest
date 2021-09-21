101a3383e9c6bdae72e4faee1c46182a0a76d3cb
claire_bouillot <claire.bouillot12@gmail.com>
2021-09-21 10:14:36 +0200

    Test

---
+++ app/src/main/java/com/example/myhearttest/MyHeartMonito.java
@@ -0,0 +1,14 @@
+package com.example.myhearttest;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MyHeartMonito extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_iam);
+    }
+}
