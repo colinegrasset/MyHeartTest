39b659814718947117ab02acf470b4dba2771a9c
coline <coline-grasset@outlook.fr>
2021-09-22 17:23:35 +0200

    creation personne

--- app/src/main/java/com/example/myhearttest/IAm.java
+++ app/src/main/java/com/example/myhearttest/IAm.java
@@ -11,4 +11,6 @@ public class IAm extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iam);
     }
+
+
 }
---
+++ app/src/main/java/com/example/myhearttest/Personne.java
@@ -0,0 +1,17 @@
+package com.example.myhearttest;
+
+public class Personne {
+    private String prenom;
+    private Boolean sexe; //homme=false et femme=true;
+    private Integer age;
+    private boolean coeur;
+    private boolean cholesterol;
+    private boolean diabetique;
+    private boolean tension;
+    private boolean famillePbCoeur;
+    private boolean IMC;
+    private boolean riskCardio;
+    private boolean coeurTest;
+    private boolean cardiologue;
+
+}
