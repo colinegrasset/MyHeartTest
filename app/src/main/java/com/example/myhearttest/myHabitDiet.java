a902f697861308e46e5a6093f8f182ade9354702
claire_bouillot <claire.bouillot12@gmail.com>
2021-09-30 11:10:37 +0200

    Modif barre du haut

---
+++ app/src/main/java/com/example/myhearttest/myHabitDiet.java
@@ -0,0 +1,14 @@
+package com.example.myhearttest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class myHabitDiet extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_habit_diet);
+    }
+}
