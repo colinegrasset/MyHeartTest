3f6a92f4f76036f021c33f3cb8bbe736d47cd87b
claire_bouillot <claire.bouillot12@gmail.com>
2021-09-30 10:52:33 +0200

    TestResults Boutons

--- app/src/main/java/com/example/myhearttest/TestResult.java
+++ app/src/main/java/com/example/myhearttest/TestResult.java
@@ -2,13 +2,24 @@ package com.example.myhearttest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 public class TestResult extends AppCompatActivity {
 
+    private String url;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_result);
+        url ="https://fedecardio.org/je-me-teste/";
+    }
+
+    public void goToSite(View view) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
     }
 }
